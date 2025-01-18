5b19e401259b2376d1dabda51a34cd18a5d01552
Zector1114 <zectorssector@gmail.com>
2025-01-18 12:26:49 -0500

    Finished the CarryOvers framework

--- Assets/Scripts/Utility/CarryOvers.cs
+++ Assets/Scripts/Utility/CarryOvers.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public static class CarryOvers
 {
+    // Add more variables for things as necessary
     public static int logNum = 0;
     public static bool multiTool = false;
+
+    // Add functions to this region as necessary
+    #region Toggles and Iterations
+    public static void LogTickUp()
+    {
+        logNum++;
+    }
+
+    public static void ToggleMultiTool()
+    {
+        multiTool = !multiTool;
+    }
+    #endregion
 }
