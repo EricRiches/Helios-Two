7a6630cc87a1b99ab26739275093a5eb6012dfc3
SunsCradle <77182764+SunsCradle@users.noreply.github.com>
2025-04-03 19:10:51 -0400

    Subtitles

--- Assets/TerminalVoicelineHandler.cs
+++ Assets/TerminalVoicelineHandler.cs
@@ -8,15 +8,27 @@ public class TerminalVoicelineHandler : MonoBehaviour
     private static bool lastTerminal;
     public StudioEventEmitter voiceLine1;
     public StudioEventEmitter voiceLIne2;
+
+    public Subtitle subtitle1;
+    public Subtitle subtitle2;
+
+    public void PlaySubtitles()
+    {
+
+    }
     public void PlayVoiceLine()
     {
         if (!lastTerminal)
         {
             voiceLine1.Play();
+            SubtitleManager.instance.SetSubtitle(subtitle1);
+            SubtitleManager.instance.PlayCurrentSubtitles();
             lastTerminal = false;
         }
         else
         {
+            SubtitleManager.instance.SetSubtitle(subtitle2);
+            SubtitleManager.instance.PlayCurrentSubtitles();
             voiceLIne2.Play();
         }
     }
